22f6adacf832e8f288df6d063bf12394871cadef
Praveen Venkatesh <praveenv253@gmail.com>
2013-10-02 17:35:55 +0530

    Add threshold-finding; draw phase portrait.
    - To implement phase portraits, I've done everything using axes, rather
      than the default pyplot.* tools. Removing lines is as easy as pie.

--- assignment2/fitzhugh-nagumo.py
+++ assignment2/fitzhugh-nagumo.py
@@ -28,7 +28,7 @@ def fwd_euler_step(v0, w0, dt, i_app, alpha, beta, gamma, tau):
     return v, w
 
 
-def fwd_euler(v0, w0, dt, i_app, t, alpha, beta, gamma, tau):
+def fwd_euler(v0, w0, dt, i_app, t, alpha, beta, gamma, tau, plot=True):
     """
     Performs a forward Euler computation to determine the time-trace of voltage
     for a given amount of time `t` in time steps of `dt`.
@@ -47,19 +47,26 @@ def fwd_euler(v0, w0, dt, i_app, t, alpha, beta, gamma, tau):
     w_nullcline = (v_axis + alpha) / beta
     v_nullcline = gamma * v_axis - v_axis**3 / 3 + i_app[0]
 
-    # Set figure and subplot properties
-    plt.ion()
-    plt.figure(0, figsize=(12, 9))
-    plt.subplot(311)
-    plt.subplot(312)
-    plt.title('Voltage trace')
-    plt.xlabel(r'$t$')
-    plt.ylabel(r'$v$')
-    plt.subplot(313)
-    plt.title('External current')
-    plt.xlabel(r'$t$')
-    plt.ylabel(r'$I_{app}$')
-    plt.tight_layout(pad=2)
+    if plot:
+        # Set figure and subplot properties
+        plt.ion()
+        fig = plt.figure(0, figsize=(12, 9))
+        phase_diagram = fig.add_subplot(311)
+        phase_diagram.set_title('Phase plane')
+        phase_diagram.set_xlabel(r'$v$')
+        phase_diagram.set_ylabel(r'$w$')
+        [vnc,] = phase_diagram.plot(v_axis, v_nullcline, 'b-')
+        [wnc,] = phase_diagram.plot(v_axis, w_nullcline, 'g-')
+        [pos,] = phase_diagram.plot(0, 0, 'ro')
+        vtrace = fig.add_subplot(312)
+        vtrace.set_title('Voltage trace')
+        vtrace.set_xlabel(r'$t$')
+        vtrace.set_ylabel(r'$v$')
+        itrace = fig.add_subplot(313)
+        itrace.set_title('External current')
+        itrace.set_xlabel(r'$t$')
+        itrace.set_ylabel(r'$I_{app}$')
+        fig.tight_layout(pad=2)
 
     for i in xrange(1, n):
         # Increment time step
@@ -68,51 +75,60 @@ def fwd_euler(v0, w0, dt, i_app, t, alpha, beta, gamma, tau):
         # Re-compute nullcline in case exciting current has changed
         if i_app[i] != i_app[i-1]:
             v_nullcline += i_app[i] - i_app[i-1]
-        # Clear the figure
         # Plot the phase plane with the nullclines and present position
-        plt.subplot(311)
-        plt.cla()
-        plt.title('Phase plane')
-        plt.xlabel(r'$v$')
-        plt.ylabel(r'$w$')
-        plt.plot(v_axis, v_nullcline, 'b-')
-        plt.plot(v_axis, w_nullcline, 'g-')
-        plt.plot(v[i], w[i], 'ro')
-        # Plot the v-trace
-        plt.subplot(312)
-        plt.plot([(i-1)*dt, i*dt], [v[i-1], v[i]], 'b-')
-        # Plot the i-trace
-        plt.subplot(313)
-        plt.plot([(i-1)*dt, i*dt], [i_app[i-1], i_app[i]], 'b-')
-        # Redraw the figure
-        plt.draw()
+        if plot:
+            # Plot the phase diagram
+            phase_diagram.lines.remove(vnc)
+            phase_diagram.lines.remove(pos)
+            [vnc,] = phase_diagram.plot(v_axis, v_nullcline, 'b-')
+            phase_diagram.plot([v[i-1], v[i]], [w[i-1], w[i]], 'k-')
+            [pos,] = phase_diagram.plot(v[i], w[i], 'ro')
+            # Plot the v-trace
+            vtrace.plot([(i-1)*dt, i*dt], [v[i-1], v[i]], 'b-')
+            # Plot the i-trace
+            itrace.plot([(i-1)*dt, i*dt], [i_app[i-1], i_app[i]], 'b-')
+            # Redraw the figure
+            plt.draw()
+
+    # Show the final plot
+    if plot:
+        plt.ioff()
+        plt.show()
 
     # Return the voltage trace
     return v
 
 
 def excitability():
-    v0 = -0.7     # Initial voltage
+    """Show excitability of the FN-neuron."""
+
+    v0 = -1.5     # Initial voltage
     w0 = -0.5     # Initial w-parameter
     dt = 0.3      # Simulation time step
     t = 100       # Total simulation time
     n = int(t / dt)
+
     # FN model parameters
     alpha = 0.7
     beta = 0.8
     gamma = 1
     tau = 1 / 0.08
     i_app = -0.5 * sp.ones(n)  # Applied current
-    i_app[0.2*n:0.3*n] = 1 * sp.ones(0.1*n)
+    i_app[0.15*n:0.2*n] = -0.3333 * sp.ones(0.05*n + 1)
+    i_app[0.4*n:0.45*n] = -0.1667 * sp.ones(0.05*n)
+    i_app[0.65*n:0.7*n] = 0 * sp.ones(0.05*n + 1)
 
     v_t = fwd_euler(v0, w0, dt, i_app, t, alpha, beta, gamma, tau)
 
 
 def spiking():
+    """Show spiking in the FN-neuron."""
+
     v0 = -0.7     # Initial voltage
     w0 = -0.5     # Initial w-parameter
     dt = 0.3      # Simulation time step
     t = 100       # Total simulation time
+
     # FN model parameters
     alpha = 0.7
     beta = 0.8
@@ -125,6 +141,8 @@ def spiking():
 
 
 def depolarization():
+    """Show depolarization in the FN-neuron."""
+
     v0 = -0.7     # Initial voltage
     w0 = -0.5     # Initial w-parameter
     dt = 0.3      # Simulation time step
@@ -137,12 +155,13 @@ def depolarization():
     gamma = 1
     tau = 1 / 0.08
     i_app = 2 * sp.ones(n)  # Applied current
-    #i_app[0.2*n:0.3*n] = 1 * sp.ones(0.1*n)
 
     v_t = fwd_euler(v0, w0, dt, i_app, t, alpha, beta, gamma, tau)
 
 
 def bistability():
+    """Show bistability in the FN-neuron."""
+
     v0 = -2       # Initial voltage
     w0 = -1       # Initial w-parameter
     i_app = 0.85  # Applied current
@@ -163,10 +182,71 @@ def bistability():
     v_t = fwd_euler(v0, w0, dt, i_app, t, alpha, beta, gamma, tau)
 
 
+def calc_firing_rate(v_trace, tot_time):
+    """Calculates the firing rate of the neuron, given a voltage trace."""
+
+    # "Spiking" happens only when at least 2 spikes are produced.
+    CONTINUITY_THRESHOLD = 2
+    SPIKING_THRESHOLD = 2
+
+    # Threshold the trace against the spiking threshold to find out where
+    # spikes have occurred.
+    thresholded_trace = sp.where(v_trace > SPIKING_THRESHOLD,
+                                 sp.ones(len(v_trace)), sp.zeros(len(v_trace)))
+
+    # Count the number of 0->1 transitions to count the number of spikes
+    # => Subtract adjacent terms
+    transitions = thresholded_trace[1:] - thresholded_trace[:-1]
+    # Still has 1->0 transitions => get rid of these
+    zero_one_transitions = sp.where(transitions == 1)
+    num_spikes = zero_one_transitions[0].size
+
+    if num_spikes < CONTINUITY_THRESHOLD:
+        return 0
+    else:
+        return float(num_spikes) / tot_time
+
+
+def thresholds():
+    """Print excitability and depolarization thresholds of the FN-neuron."""
+
+    v0 = -0.7     # Initial voltage
+    w0 = -0.5     # Initial w-parameter
+    dt = 0.3      # Simulation time step
+    t = 100       # Total simulation time
+    n = int(t / dt)
+
+    # FN model parameters
+    alpha = 0.7
+    beta = 0.8
+    gamma = 1
+    tau = 1 / 0.08
+    currents = sp.linspace(-0.5, 1.5, 100)
+
+    excitability_threshold = None
+    depolarization_threshold = None
+
+    for i in currents:
+        i_app = i * sp.ones(n)  # Applied current
+        v_t = fwd_euler(v0, w0, dt, i_app, t, alpha, beta, gamma, tau, False)
+        f = calc_firing_rate(v_t, t)
+        if f and excitability_threshold is None:
+            excitability_threshold = i
+        if( f == 0 and excitability_threshold is not None
+                   and depolarization_threshold is None ):
+            depolarization_threshold = i
+
+    print 'Excitability threshold: %f' % excitability_threshold
+    print 'Depolarization threshold: %f' % depolarization_threshold
+
+    return excitability_threshold, depolarization_threshold
+
+
 def usage(prog_name):
     print('Usage:')
     print('    ' + str(prog_name) + ' --excitability | --spiking |'
-                                    ' --bistability | --depolarization')
+                                    ' --bistability | --depolarization |'
+                                    ' --thresholds')
 
 
 if __name__ == '__main__':
@@ -182,5 +262,7 @@ if __name__ == '__main__':
         bistability()
     elif sys.argv[1] == '--depolarization':
         depolarization()
+    elif sys.argv[1] == '--thresholds':
+        thresholds()
     else:
         usage(sys.argv[0])
